72919d7018deb03099f5cdfbc0d93e0e15926ee4
andresousapt <andresousa.net@gmail.com>
2019-02-16 11:34:13 +0000

    Header and Nav end.

---
+++ js/webconfig.js
@@ -0,0 +1,93 @@
+window.onload = function(){
+    //code for manipulating DOM
+}
+
+
+//Regist Participants
+//JavaScript - Open Modal Window
+const btnRegister = document.getElementById("btnRegister")
+// Registar participante
+/* btnRegister.addEventListener("click", function() {
+    swal({
+      title: "Inscrição na WebConference",
+      html:
+      '<input id="swal-input1" class="swal2-input" placeholder="nome">' +
+      '<input id="swal-input2" class="swal2-input" placeholder="e-mail">',      
+      showCancelButton: true,
+      confirmButtonText: "Inscrever",
+      cancelButtonText: "Cancelar",
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        const name = document.getElementById('swal-input1').value
+        const email = document.getElementById('swal-input2').value
+        return fetch(`${urlBase}/conferences/1/participants/${email}`, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },          
+          method: "POST",
+          body: `nomeparticipant=${name}`
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.json();
+          })
+          .catch(error => {
+            swal.showValidationError(`Request failed: ${error}`);
+          });
+      },
+      allowOutsideClick: () => !swal.isLoading()
+    }).then(result => {
+      if (result.value) {               
+        if (!result.value.err_code) {
+          swal({title: "Inscrição feita com sucesso!"})  
+        } else {
+          swal({title: `${result.value.err_message}`})  
+        }
+      }
+    });
+  }); */
+ btnRegister.addEventListener("click",function(){
+    swal({
+        title:"WebConference Subscription", 
+        html:
+        '<input id="txtName" class="swal2-input" placeholder="name">' + 
+        '<input id="txtEmail" class="swal2-input" placeholder="e-mail">', 
+        showCancelButton:true, 
+        confirmButtonText:"Confirm", 
+        cancelButtonText:"Cancel",
+        showLoaderOnConfirm:true, 
+        preConfirm: () => {
+             const name = document.getElementById('textName').value
+             const email = document.getElementById('txtEmail').value
+             const url_base = "https://fcawebbook.herokuapp.com"
+             return 
+             fetch('${url_base}/conference/1/participants/${email}', {
+                 headers: {
+                     "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                 method: "POST",
+                 body: 'nomeparticipant=${name}'
+             })
+             .then(response => {
+                 if(!response.ok){
+                 throw new Error(response.statusText);
+             }
+             return response.json();
+            })
+            .catch (error => {
+                swal.showValidationError('Request failed: ${error}');
+            });
+         },
+         allowOutsideClick: () => !sawl.isLoading()
+    }).then(result => {
+        if(result.value){
+            if(!result.value.err_code){
+                swal({title: "Success Subscription!"})
+            } else{
+                swal({title: '${result.value.err_message'})
+            }
+        }
+    })  
+}) 
